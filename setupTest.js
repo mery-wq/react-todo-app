230890a39cddf63c4e5792b16b945a9bb5f75ee7
yut-wq <println856@gmail.com>
2024-09-14 05:36:27 +0000

    chore: add vitest for unit test.

---
+++ setupTest.js
@@ -0,0 +1,5 @@
+// import '@testing-library/jest-dom/vitest';
+// import { expect } from "vitest";
+// import matchers from "@testing-library/jest-dom/matchers";
+
+// expect.extend(matchers);
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+/// <reference types="vitest" />
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  test: {
+    // global: true,
+    environment: "jsdom",
+    setupFiles: ["./vitest.setup.ts"],
+  },
   plugins: [react()],
   server: {
     watch: {
       usePolling: true,
-      interval: 1000
-    }
-  }
-})
+      interval: 1000,
+    },
+  },
+});
